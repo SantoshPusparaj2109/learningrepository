ad7d1f68679e55449c0374ef6accfcdfcc34fefc
Santosh Pusparaj <ssantoshpusparaj2109@gmail.com>
2023-04-11 22:21:30 +0530

    Removed default two

--- version.tf
+++ version.tf
@@ -1,22 +1,20 @@
-terraform {
-  #   cloud {
-  #     organization = "development-env"
+# terraform {
+#   #   cloud {
+#   #     organization = "development-env"
 
-  #     workspaces = {
-  #       name = "learningrepository"
-  #     }
-  #   }
-  required_version = "~> 1.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.62"
-    }
-  }
-}
+#   #     workspaces = {
+#   #       name = "learningrepository"
+#   #     }
+#   #   }
+#   required_version = "~> 1.3"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.62"
+#     }
+#   }
+# }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "us-east-1"
 }
